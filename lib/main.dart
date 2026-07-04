d6b0984ec84c633058248425e0a12265390e6260
alamsamir7666-ux <alamsamir7666@gmail.com>
2026-07-04 14:44:25 +0600

    Integrate Firebase: google-services.json via secret, Firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp();
   await Hive.initFlutter();
   await Hive.openBox('blockit_settings');
   await di.init();
+  
   runApp(const BlockitApp());
 }
